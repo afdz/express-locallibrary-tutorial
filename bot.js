c21bba0e8f1d153de586bc9cfafc21e0df027a71
Agus <afernanc@everis.com>
2018-08-31 14:19:28 +0200

    Bot demo server

--- bot.js
+++ bot.js
@@ -1,16 +1,26 @@
-const TelegramBot = require('node-telegram-bot-api');
-
 // API Token Telegram
 const token = '468346940:AAG7gFh1nIeJuZkuj81E1EmzIj1Y76-pFJg';
 
-// Creamos un bot que usa 'polling'para obtener actualizaciones
-const bot = new TelegramBot(token, {polling: true});
-const request = require('request');
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
 
-// Cuando mandes el mensaje "Hola" reconoce tú nombre y genera un input: Hola Daniel
-bot.on('message', (msg) => {
-    var Hola = "hola";
-    if (msg.text.toString().toLowerCase().indexOf(Hola) === 0) {
-        bot.sendMessage(msg.chat.id, "Hola  " + msg.from.first_name);
-    }
+console.log('bot server started...');
+
+// hello command
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+// sum command
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+  var result = 0;
+  match[1].trim().split(/\s+/).forEach(function (i) {
+    result += (+i || 0);
+  })
+  bot.sendMessage(msg.chat.id, result).then(function () {
+    // reply sent!
+  });
 });
